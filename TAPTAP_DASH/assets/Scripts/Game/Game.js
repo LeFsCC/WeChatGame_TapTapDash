ae57e33e6276faf048aac218b5bc4905f7a9f0a8
MarcoSSJ <sunsj16@mails.tsinghua.edu.cn>
2019-07-17 10:39:48 +0800

    小心翼翼的尝试

--- TAPTAP_DASH/assets/Scripts/Game/Game.js
+++ TAPTAP_DASH/assets/Scripts/Game/Game.js
@@ -2,20 +2,85 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-
+        scoreTxt: cc.RichText,
+        timeTxt: cc.RichText
     },
-    onLoad () {
+    editor: {
+        executionOrder: 0
+    },
+    onLoad() {
         this.initSource()
+        this.registEventHandler()
     },
-
-    start () {
-
+    start() {
+        this.initGame()
     },
+    // 绑定一些节点
     initSource: function() {
-        cc.log('init game')
         this.background = this.node.getChildByName('background')
         this.gameCamera = this.node.getChildByName('gameCamera')
-        this.gameCamera.active = true
+        this.scoreNode = this.node.getChildByName('score')
+        this.ptimerNode = this.node.getChildByName('time')
+        this.pauseBn = this.node.getChildByName('pauseBn')
+
+        // 场景宏观变量
+        this.sceneState = 'pause' // 当前场景处于暂停状态
+        this.timeSpan = 0 // 已经经过的时间
+        this.if_touch = false
+    },
+    // 刷新时间函数
+    updateTime: function() {
+        // 如果游戏不在运行中，就不再刷新
+        if (!this.sceneState)
+            return
+
+        this.timeSpan += 1
+        let minute = Math.floor(this.timeSpan / 60)
+        let second = Math.floor(this.timeSpan % 60)
+        let string1 = String(minute)
+        let string2 = String(second)
+        if (string1.length == 1) {
+            string1 = '0' + string1
+        }
+        if (string2.length == 1) {
+            string2 = '0' + string2
+        }
+        this.timeTxt.string = String('<color=#000000>' + string1 + '：' + string2 + '</color>')
+    },
+    // 初始化游戏并重新开始，所有属性归到初始状态
+    initGame: function() {
+        this.timeSpan = 0
+            // TODO: 初始化道路和人物
+        this.gameCamera.getComponent('Controller').launchGame()
+        this.schedule(function() {
+            this.updateTime()
+        }, 1)
+    },
+    // 暂停或回到游戏，地图和人物都不动
+    pause_back_Game: function() {
+        console.log('pause ')
+        if (this.sceneState === true) {
+            this.sceneState = false
+            this.gameCamera.getComponent('Controller').pauseMap()
+        } else {
+            this.sceneState = true
+            this.gameCamera.getComponent('Controller').restartMap()
+        }
+        this.pauseBn.getComponent('ChangePauseBn').changePic(this.sceneState)
+    },
+    // 改变camera 的角度，如果在修改camera角度的时候人物位置发生了异常，那么游戏结束
+    changeRotation: function() {
+        if (this.if_touch === false) {
+            this.if_touch = true
+            this.gameCamera.getComponent('Controller').RotateCamera()
+        }
+    },
+    closeTouch: function() {
+        this.if_touch = false
+    },
+    registEventHandler: function() {
+        this.pauseBn.on('click', this.pause_back_Game, this)
+        this.node.on('touchstart', this.changeRotation, this)
+        this.node.on('touchend', this.closeTouch, this)
     }
-    // update (dt) {},
 });
